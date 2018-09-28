7232a923f3cf3e27683857d3dece64ce2ed258cd
Mo <42975535+MoIbrahim21@users.noreply.github.com>
2018-09-28 12:27:33 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @author mibrahim21
+ *
+ */
+public class OperationsTest {
+
+	public static void main(String[] args) 
+	{
+		int number; // Declararion statment
+		number = 20; //assignment statment -> initialization 
+		number = number + 5;
+		System.out.println(number);
+		number = number/2; 
+		System.out.println(number);
+		number = number * 4;
+		System.out.println(number);
+		number = 2% number;
+		System.out.println(number);	
+	}
+	
+
+}
